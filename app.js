2355780651b9a2cbfdde3859c1cb6395c3edb21e
Fayyaz Alam <fayazalam.98@gmail.com>
2021-07-28 13:56:24 -0700

    redo with es6

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-var main =document.getElementById('main')
-var regex=/(?<==)(.*?)(?=&)/;  
-var link= 'https://newsapi.org/v2/everything?q=apple&from=2021-07-25&to=2021-07-25&sortBy=popularity&apiKey=0b8d9a0f6ab743af843f2c3549454a7c';
 
-  var search=document.getElementById("srch").value;
-  var newStr = link.replace(regex, search);
-  fetch(link)
-  .then(response => response.json())
-  .then(json  =>{
-    // console.log(json.articles.length)
-     
-    for(var i=0; i<json.articles.length;i++){
+let main =document.getElementById('main');
+let srch =document.getElementById("srch");
+let allData;
+
+let getData = (search)=>{
+  return new Promise((resolve,reject)=>{
+    fetch(`https://newsapi.org/v2/everything?q=${search}&from=2021-06-26&sortBy=publishedAt&apiKey=e17833fa821c47af88ba8930480962b1`)
+    .then(data =>data.json())
+    .then (data =>{
+     resolve(data) 
+    })
+  })
+}
+
+let setData = async()=>{
+  let a = await getData(srch.value);
+
+  
+
+  for(var i=0; i<a.articles.length;i++){
     
-        let card=`<div class="card  col-md-4  col-sm-6 " >
-        <img class="card-img-top width="100%" src="${json.articles[i].urlToImage}" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">${json.articles[i].author}</h5>
-          <p class="card-text">${json.articles[i].description}</p>
-          <p class="card-text">${json.articles[i].content}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${json.articles[i].publishedAt}</li>
-        </ul>
-      
-      </div>`
+            let card=`<div class="card  col-md-4  col-sm-6 " >
+            <img class="card-img-top width="100%" src="${a.articles[i].urlToImage}" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title">${a.articles[i].author}</h5>
+               <p class="card-text">${a.articles[i].description}</p>
+               <p class="card-text">${a.articles[i].content}</p>
+             </div>
+             <ul class="list-group list-group-flush">
+               <li class="list-group-item">${a.articles[i].publishedAt}</li>
+             </ul>      
+           </div>`
+// console.log(card)
+          main.innerHTML += card;
+        }
+}
 
-      main.innerHTML += card;
-    }
-} )
-
-
-
-
-
-
-
-
-
+setData()
